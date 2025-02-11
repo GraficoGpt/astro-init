0f161fd7be048f51703fdb94e9b67a027b62d403
kashipu <graficowm@gmail.com>
2025-02-11 15:49:46 -0500

    Astro con docker ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'; // Asegúrate de que esta línea esté correctamente importada
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com', // Cambia por tu URL real
+  output: 'server',
+  adapter: node({
+    mode: 'standalone', // Usa el modo standalone para Railway
+  }),
+});
+
